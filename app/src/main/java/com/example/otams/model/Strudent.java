bfca4f57a26110ed0a5f74ddc2ab15c5c35bac08
bjens <bjens081@uottawa.ca>
2025-10-13 18:52:18 -0400

    commit files

--- app/src/main/java/com/example/otams/model/Strudent.java
+++ app/src/main/java/com/example/otams/model/Strudent.java
@@ -1,12 +1,12 @@
 package com.example.otams.model;
 
-public class Student {
+public class Student extends User {
     private String firstName;
     private String lastName;
     private String phoneNumber;
     private String programOfStudy;
 
-    public Student(String firstName, String lastName, String phoneNumber, String programOfStudy) {
+    public Student(String firstName, String lastName, String phoneNumber, String programOfStudy, String email, String password) {
         super(email, password);
         this.firstName = firstName;
         this.lastName = lastName;
