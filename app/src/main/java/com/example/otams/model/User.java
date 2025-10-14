b658f6d0f76409387d6f571ef9de997888132fe7
anthonytraboulsi <traboulsianthony@gmail.com>
2025-10-13 20:38:09 -0400

    Administrator

--- app/src/main/java/com/example/otams/model/User.java
+++ app/src/main/java/com/example/otams/model/User.java
@@ -1,18 +1,27 @@
 package com.example.otams.model;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class User {
     private String email;
     private String password;
+    private String userLabel;
 
-    public User(String email, String password) {
-        this.email = email;
+    //To save in memory
+    public static final List<User> Users = new ArrayList<>();
+
+    public User(String email, String password, String userLabel) {
+        this.email = email.trim().toLowerCase();
         this.password = password;
+        this.userLabel = userLabel;
     }
-
     public String getEmail() {
         return email;
     }
     public String getPassword() {
         return password;
+    }
+    public String welcomeMessage() {
+        return "Welcome! You are logged in as " + userLabel;
     }
 }
