108fd03246eff6b85a88e106bd3db91c1e7059bb
anthonytraboulsi <traboulsianthony@gmail.com>
2025-10-27 01:25:59 -0400

    OCT 27

--- app/src/main/java/com/example/proj16v2/RegisterTutor.java
+++ app/src/main/java/com/example/proj16v2/RegisterTutor.java
@@ -16,7 +16,7 @@ public class RegisterTutor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_registertutor);
+        setContentView(R.layout.activity_register_tutor);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
