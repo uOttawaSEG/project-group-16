e4b0fbdd233886f39e42d9cad20e085069ca6101
Kr3st3r <ksams047@uottawa.ca>
2025-10-27 23:52:38 -0400

    "fixed main and register"

--- app/src/main/java/com/example/proj16v2/MainActivity.java
+++ app/src/main/java/com/example/proj16v2/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
