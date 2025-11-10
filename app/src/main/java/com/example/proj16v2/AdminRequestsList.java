fa99c9d8c9d9f2a7d68d8843c0fad6e92d9f353b
bjens <bjens081@uottawa.ca>
2025-11-09 20:34:46 -0500

    Initial Android Studio project setup for OTAMS Deliverable

--- app/src/main/java/com/example/proj16v2/AdminRequestsList.java
+++ app/src/main/java/com/example/proj16v2/AdminRequestsList.java
@@ -1,24 +1,37 @@
 package com.example.proj16v2;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Admin_list extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminRequestsList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_list);
+        setContentView(R.layout.activity_admin_requests_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+// one-liner SQL you can use in query():
+// SELECT user_id, first_name, last_name, email, user_role, registration_status
+// FROM Users WHERE registration_status=? AND user_role IN ('Student','Tutor')
+
+
+// UPDATE Users SET registration_status=? WHERE user_id=?
 }
